5d1de900730574578e23b97c9bcb7340304706ee
Vahidvhd <vahidvhd91@gmail.com>
2026-07-08 17:05:41 +0100

    Add category and product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    Category=models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name='products'
+    )
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    is_active = models.BooleanField(default= True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
