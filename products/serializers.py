9be7b3329340803f735569abcc905b0a12fb80b1
Vahidvhd <vahidvhd91@gmail.com>
2026-07-08 19:55:35 +0100

    Add serializer validation for categories and products

--- products/serializers.py
+++ products/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Category, Product
 
 class CategorySerializer(serializers.ModelSerializer):
+    name = serializers.CharField(min_length=3, max_length=100, trim_whitespace=True)
     class Meta:
         model = Category
         fields = ['id', 'name', 'created_at']
@@ -10,8 +11,12 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     category_detail = CategorySerializer(source = 'category', read_only = True)
+    name = serializers.CharField(min_length=3, max_length=100, trim_whitespace=True)
+    price= serializers.IntegerField(min_value = 1)
+
 
     class Meta:
         model = Product
         fields = ['id', 'name', 'price', 'is_active', 'category', 'category_detail', 'created_at']
         read_only_fields = ['id', 'created_at']
+        
