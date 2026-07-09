d37bae58f1553e3aeab8f8fc3a671f363b93657c
Vahidvhd <vahidvhd91@gmail.com>
2026-07-09 17:19:14 +0100

    Add custom permissions for product APIs

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -9,6 +9,7 @@ from .serializers import CategorySerializer, ProductSerializer
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset= Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     filter_backends = [                           
         filters.SearchFilter,
@@ -24,6 +25,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset= Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     filter_backends = [ 
         DjangoFilterBackend,                          
